6837444d7a0fb37878b56d521a2adf4b1111373b
jimpoul7 <jimpoul7@gmail.com>
2016-12-04 20:10:33 +0200

    Added mobility, prevent endgame pawn army

--- evaluation.c
+++ evaluation.c
@@ -2,28 +2,35 @@
 
 int Eval(S_BOARD *pos){
 
+    int material_difference;
+    int attacked_squares;
+    int mobility;
 
-  return  50*(CountBits(CaptureSquares(pos,BLACK)) - CountBits(CaptureSquares(pos,WHITE))) + pos->material[WHITE] - pos->material[BLACK];
+    material_difference = pos->material[WHITE] - pos->material[BLACK];
+    attacked_squares = CountBits(CaptureSquares(pos,BLACK)) - CountBits(CaptureSquares(pos,WHITE));
+    mobility = Mobility(pos,WHITE) -  Mobility(pos,BLACK);
+
+    return  15*attacked_squares + material_difference + 15*mobility + 1000*(pos->bitboards[bP]==pos->occupied[BLACK]) -1000*(pos->bitboards[wP]==pos->occupied[WHITE]);
 
 }
 
 int Mobility(S_BOARD *pos, int side){
 
-  uint64_t attacked;
+    uint64_t attacked;
 
-  if(side == WHITE){
-    // Black pieces attacked by WHITE
-    attacked = CaptureSquares(pos,WHITE) & pos->occupied[BLACK];
+    if(side == WHITE){
+        // Black pieces attacked by WHITE
+        attacked = CaptureSquares(pos,WHITE) & pos->occupied[BLACK];
 
-    if(!CountBits(attacked)) return CountBits(MoveSquares(pos,WHITE));
-    else return CountBits(attacked);
-  }
-  else{
-    // White pieces attacked by BLACK
-    attacked = CaptureSquares(pos,BLACK) & pos->occupied[WHITE];
+        if(!CountBits(attacked)) return CountBits(MoveSquares(pos,WHITE));
+        else return CountBits(attacked);
+    }
+    else{
+        // White pieces attacked by BLACK
+        attacked = CaptureSquares(pos,BLACK) & pos->occupied[WHITE];
 
-    if(!CountBits(attacked)) return CountBits(MoveSquares(pos,BLACK));
-    else return CountBits(attacked);
-  }
+        if(!CountBits(attacked)) return CountBits(MoveSquares(pos,BLACK));
+        else return CountBits(attacked);
+    }
 
 }
